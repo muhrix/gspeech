855707f082188240771a20d2c545799a076232ee
Murilo FM <muhrix+pluto@gmail.com>
2013-12-27 23:45:03 -0200

    Working version

--- gspeech/scripts/gspeech.py
+++ gspeech/scripts/gspeech.py
@@ -2,28 +2,28 @@
 #
 # Author: Murilo FM
 # Email:  muhrix@gmail.com
-# 14 Dec 2013
+# 27 Dec 2013
 # Based on the code written by:
 #    achuwilson (https://github.com/achuwilson/gspeech)
 #    korylprince (https://github.com/korylprince/python-google-transcribe)
 #
 
 
-PACKAGE='gspeech'
+#PACKAGE='gspeech'
 import rospy
 from std_msgs.msg import String
-from std_msgs.msg import Int8
+from std_msgs.msg import Float32
 from gspeech_msgs.msg import Speech
 
 import sys
 import urllib2
 import json
-import os
+#import os
+import subprocess
+#import shlex
 
-import shlex,subprocess
-
-cmd1='sox -r 16000 -t alsa default recording.flac silence 1 0.1 1% 1 1.5 1%'
-cmd2='wget -q -U "Mozilla/5.0" --post-file recording.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-GB&client=chromium"'
+cmd1='sox -r 16000 -t alsa default recording.flac silence 1 0.1 60% 1 1.5 60%'
+cmd2='wget -q -U "Mozilla/5.0" --post-file recording.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang=en-GB"'
 
 # For other language acronyms available for use with Google API, see:
 # http://stackoverflow.com/questions/14257598/what-are-language-codes-for-voice-recognition-languages-in-chromes-implementati
@@ -31,21 +31,44 @@ language = 'en-GB' # en-US, pt-BR, pt-PT, ...
 url_req = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&lang='+language
 
 def speech():
-	rospy.init_node('gspeech')
-        msg = Speech()
-        pubm = rospy.Publisher('spoken_line', Speech)
+	#rospy.init_node('gspeech')
+    msg = Speech()
 
-	os.system(cmd1)
-	req = urllib2.Request(url_req, data=recording.flac, headers={'Content-type': 'audio/x-flac; rate=16000'})
+    try:
+        ret = subprocess.call(cmd1, shell=True)
+        if ret < 0:
+            return
+    except OSError as e:
+        rospy.logerror('Command execution failed')
+        return
+    
+    f = open('recording.flac')
+    data = f.read()
+    f.close()
+    
+    req = urllib2.Request(url_req, data, headers={'Content-type': 'audio/x-flac; rate=16000'})
 
-	try:
-		ret = urllib2.urlopen(req)
-	except urllib2.URLError:
-		# print ROS_ERROR and carry on
-		rospy.logerror('Could not transcribe spoken line')
+    try:
+        ret = urllib2.urlopen(req)
+    except urllib2.URLError, e:
+        # print ROS_ERROR and carry on
+        rospy.logerror('urllib2 request error: %s'%e.reason)
+        
+    json_msg = json.loads(ret.read())
+    print json_msg
 
-	text = json.loads(ret.read())['hypotheses'][0]['utterance']
-	print text
+    # status = 0 means something was recognised
+    # it appears that status = 5 means that nothing could be
+    # recognised, but sox recording and urllib2 requests worked
+    if json_msg['status'] == 0:
+        msg.speech = json_msg['hypotheses'][0]['utterance']
+        msg.confidence = json_msg['hypotheses'][0]['confidence']
+        pubm.publish(msg)
+        #text = json_msg['hypotheses'][0]['utterance']
+        #print text
+    else if json_msg['status'] == 5:
+        rospy.loginfo('Could not transcribe spoken line')
+            
 
 #	pubs = rospy.Publisher('speech', String)
 #	pubc = rospy.Publisher('confidence', Int8)
@@ -68,13 +91,16 @@ def speech():
 #			pubc.publish(confidence)
 #			print String(data), confidence
 	
-	speech()	
+    speech()	
 	
 
 
 if __name__ == '__main__':
-	try:
-		speech()
-	except rospy.ROSInterruptException: pass
-	except KeyboardInterrupt:
-		sys.exit(1)   
+    try:
+        rospy.init_node('gspeech')
+        pubm = rospy.Publisher('spoken_line', Speech)
+        speech()
+    except rospy.ROSInterruptException:
+        pass
+    except KeyboardInterrupt:
+        sys.exit(1)
